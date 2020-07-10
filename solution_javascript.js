b3e6e1fb15e5490ec299e9f977acbcbf2eb6633b
mehwish-arif <44465887+mehwish-arif@users.noreply.github.com>
2020-07-10 14:58:58 -0400

    Update solution_javascript.js

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,58 @@
+//student
+//fallcoop2020
+
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.state = [0];
+    this.index = 0;
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    const result = this.value + num;
+    this.state.push(result);
+    this.index = this.index + 1;
+    this.value = result;
+  }
+  subtract(num) {
+    const result = this.value - num;
+    this.index = this.index + 1;
+    this.state.push(result);
+    this.value = result;
+  }
+  undo() {
+    let currentIndex = this.index - 1;
+    if(currentIndex < 0){
+      currentIndex = 0;
+    }
+    this.index = currentIndex;
+    const result = this.state[currentIndex];
+    this.value = result;
+  }
+  redo() {
+    let currentIndex = this.index + 1;
+    if(currentIndex > this.state.length) {
+      currentIndex = this.state.length - 1
+    } else {
+      this.index = currentIndex;
+    }
+
+    const result = this.state[currentIndex];
+    this.value = result;
+  }
+  bulk_undo(num) {
+    const currentIndex = this.index - num;
+    this.index = currentIndex;
+    const result = this.state[this.currentIndex];
+    this.value = result;
+
+  }
+  bulk_redo(num) {
+    const currentIndex = this.index + num;
+    this.index = currentIndex;
+    const result = this.state[this.currentIndex];
+    this.value = result;
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
